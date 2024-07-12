1d365109dc93e6a479acb87485605e05cc4af4a0
ivarlon <ivarlon@uio.no>
2024-07-12 12:00:10 +0200

    created run.py so that imports from various paths can be handled properly

---
+++ run.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Sets the correct work environment for the code.
+"""
+
+import sys
+from pathlib import Path
+root = Path(__file__).resolve().parent
+sys.path.append(str(root))
+sys.path.append(str(root / "utils"))
+
+def run_script(script_path):
+    with open(script_path) as file:
+        exec(file.read(), globals())
+
+if __name__ == "__main__":
+    import argparse
+    
+    parser = argparse.ArgumentParser(description = "Runs a script with the necessary paths set up.")
+    parser.add_argument("script", help="The script to run, relative to the root directory")
+    
+    args = parser.parse_args()
+    
+    script_path = root / args.script
+    run_script(script_path)
