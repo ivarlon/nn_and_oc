f5b9cca240ab75a5f6c43859b87917bb59eac594
ivarlon <ivarlon@uio.no>
2024-07-16 22:00:44 +0200

    Enabled a conv architecture in the DON branch net

--- DeepONet.py
+++ DeepONet.py
@@ -13,17 +13,20 @@ class DeepONet(torch.nn.Module):
                  num_out_channels=1,
                  activation_branch=torch.nn.ReLU(),
                  activation_trunk=torch.nn.Sigmoid(),
+                 n_conv_layers=0,
                  use_dropout=False,
                  final_activation_trunk=True
                  ):
         """
         input_size_branch (int): size of *flattened* branch input u; equal to num_input_channels*num_input_points
         input_size_trunk (int): size of trunk input x
-        branch_architecture (list): defines the size of each layer in branch net
+        branch_architecture (list): defines the size of each fully connected layer in branch net
         trunk_architecture (list): defines the size of each layer in trunk net
         num_out_channels (int): the size of the DeepONet output (1 for scalar outputs)
         activation_branch (element-wise function): activation function between branch layers
         activation_trunk (element-wise function): activation function between trunk layers
+        n_von_layers (int): the number of convolution layers in the branch net
+        use_dropout (bool): whether or not to use dropout for training
         final_activation_trunk (bool): whether or not to apply an activation function to the trunk output
         """
         
@@ -31,10 +34,33 @@ class DeepONet(torch.nn.Module):
         
         super().__init__()
         
+        # check if convolutional branch architecture is to be used
+        use_convolution = n_conv_layers > 0
+        if use_convolution:
+            # create conv layers that successively double the number of channels and halve the size per channel
+            # consider having input_size_branch either int for u(x) or tuple for u(t,x)
+            # then possibly do flatten for fully connected net (n_conv_layers=0)
+            if input_size_trunk == 1:
+                pool = torch.nn.MaxPool1d(kernel_size=2)
+                conv_layer = lambda i: torch.nn.Conv1d(2**i, 2**(i+1), kernel_size=3, padding=1)
+            def init_conv_net():
+                conv_layers = []
+                for i in range(n_conv_layers):
+                    conv_layers.append(conv_layer(i))
+                    conv_layers.append(activation_branch)
+                    conv_layers.append(pool)
+                conv_net = torch.nn.Sequential(*conv_layers)
+                return conv_net
         # intialise a branch net for each output dimension
+        
+        
+        
         branch_nets = []
         for channel in range(num_out_channels):
-            branch_layers = [torch.nn.Flatten()]
+            branch_layers = [torch.nn.Flatten(start_dim=1 + use_convolution, end_dim=-1)]
+            if use_convolution:
+                branch_layers.append(init_conv_net())
+                branch_layers.append(torch.nn.Flatten()) # flattens channel dimension and produces a vector of dim 2^n*input_size_branch/2^n = input_size branch (doubling channels and 2-pooling cancel)
             branch_layers.append(torch.nn.Linear(input_size_branch, branch_architecture[0]))
             if len(branch_architecture)>1:
                 for l in range(1, len(branch_architecture)):
@@ -58,10 +84,17 @@ class DeepONet(torch.nn.Module):
             trunk_layers.append(activation_trunk)
         self.trunk = torch.nn.Sequential(*trunk_layers)
         
+        # final bias
         self.bias = torch.nn.Parameter(torch.zeros(1,1,num_out_channels))
         
         # set forward method so that trunk input tensor x has same first dimension as branch input u
-        self.trunk_input_shares_first_dimension_with_branch_input(True)
+        #self.trunk_input_shares_first_dimension_with_branch_input(True)
+        
+        if use_convolution:
+            self.forward = self.forward_conv
+        else:
+            self.forward = self.forward_fc
+        
         
     def trunk_input_shares_first_dimension_with_branch_input(self, share):
         """
@@ -74,13 +107,23 @@ class DeepONet(torch.nn.Module):
         else:
             self.forward = self.forward_dont_share
             
-    def forward_share(self, u, x):
+    def forward_conv(self, u, x):
+        # u is tensor representing n-valued function evaluated at n_u points, with shape (no. of function samples, n_u*n)
+        # x is tensor representing point in R^m, with shape (no.of function samples, no. of input points, m)
+        u = u.unsqueeze(1) # add channel dim (axis 1) of size 1
+        u = torch.stack([branch(u) for branch in self.branch], dim=1) # produces a latent vector for each output dimension (keeping batch as first dimension)
+        x = self.trunk(x)
+        # B: function batch; y: output channel; i: latent dimension; b: domain batch
+        out = torch.einsum('Byi, Bbi -> Bby', u, x) + self.bias
+        return out
+    
+    def forward_fc(self, u, x):
         # u is tensor representing n-valued function evaluated at n_u points, with shape (no. of function samples, n_u*n)
         # x is tensor representing point in R^m, with shape (no.of function samples, no. of input points, m)
         u = torch.stack([branch(u) for branch in self.branch], dim=1) # produces a latent vector for each output dimension (keeping batch as first dimension)
         x = self.trunk(x)
         # B: function batch; y: output channel; i: latent dimension; b: domain batch
-        out = torch.einsum('Byi..., Bbi... -> Bby...', u, x) + self.bias
+        out = torch.einsum('Byi, Bbi -> Bby', u, x) + self.bias
         return out
     
     def forward_dont_share(self, u, x):
