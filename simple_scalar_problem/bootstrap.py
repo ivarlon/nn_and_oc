33e68fe34ce16cc3e15c146741b90c04c0a74a4f
ivarlon <ivarlon@uio.no>
2024-09-17 19:23:13 +0200

    .

--- simple_scalar_problem/bootstrap.py
+++ simple_scalar_problem/bootstrap.py
@@ -13,8 +13,10 @@ from generate_data import solve_state_eq, solve_adjoint_eq
 from OC_simple_scalar_problem import OC_problem
 from utils.bootstrap_OC import *
 
+model_name = "DON" # NN model to use: FNO or DON
+
 # create data directory to store results
-data_dir_name = 'bootstrap_results'
+data_dir_name = 'bootstrap_results_{}'.format(model_name)
 problem_dir_name = "simple_scalar_problem"
 script_dir = os.path.dirname(os.path.abspath(__file__))
 problem_dir = os.path.join(script_dir, problem_dir_name)
@@ -26,7 +28,6 @@ if not os.path.exists(data_dir):
 N = 128
 nu = 5e-3
 
-model_name = "DON" # NN model to use: FNO or DON
 
 state_filename = glob.glob(os.path.join(script_dir, 'state_experiments_{}_15_models'.format(model_name), 'models_list*.pkl'))[0]
 
@@ -34,11 +35,10 @@ adjoint_filename = glob.glob(os.path.join(script_dir, 'adjoint_experiments_{}_15
 
 NO_problem = OC_problem("NN", "NN adjoint", nu, None, None, model_name)
 
-u0 = np.zeros(shape=(1,N,1))
-
+u0 = np.zeros(shape=(1,N,1)) # initial guess
 max_no_iters = 20 # max. no. of optimisation iterations
 B = 1000 # number of boostraps
-n_models_max = 10
+n_models_max = 10 # max number of models in ensemble
 
 cost_stats, cost_true_stats, u_opt_stats = bootstrap_OC(state_filename, adjoint_filename, NO_problem, u0, max_no_iters=max_no_iters, n_models_max=n_models_max, B=B)
 
